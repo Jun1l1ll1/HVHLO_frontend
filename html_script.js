4c927e0b24f9cd5d780c1c9558a5393567b09ed2
Jun1l1ll1 <jujoa007@osloskolen.no>
2025-01-06 21:41:54 +0100

    Added booleans to show hospital status and picture form

--- html_script.js
+++ html_script.js
@@ -3,13 +3,29 @@ function getTableData() {
     
     change_hospital_name(HOSPITAL_NAME);
 
-    // Set hexagon colors
-    let side_counter = 0
-    for (let key in QUESTIONS_DATA) {
-        if (side_counter < 6) { // Hexagon has 6 sides
-            document.getElementById("hex"+side_counter).style.fill = "var(--"+QUESTIONS_DATA[key]+"_color)"
+    if (SHOW_HOSPITAL_STATUS) {
+        // Add hospital-hexagon
+        document.getElementById("hospital_stat_cont").innerHTML = `
+            <a href="./hospital.html" class="btn" id="edit_hospital_status">Rediger sykehus status</a>
+            <svg id="hexagon" width="60px" stroke="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="610,122.5,700,700">
+                <!-- Sets colors in JS -->
+                <path fill="var(--cont_color_light)" id="hex0" d="M1129.625,179.1339l-169.75064,292.71548l-168.49479,-292.71547z"/>
+                <path fill="var(--cont_color_light)" id="hex1" d="M1129.34359,179.1883l-170.35577,292.36611l339.47849,0.56304z"/>
+                <path fill="var(--cont_color_light)" id="hex2" d="M959.64785,471.63011l-169.15306,292.58537l337.28211,-0.48646z"/>
+                <path fill="var(--cont_color_light)" id="hex3" d="M1297.24543,471.5l-169.75064,292.71548l-168.49187,-293.70958z"/>
+                <path fill="var(--cont_color_light)" id="hex4" d="M960.24543,471.5l-169.75064,292.71548l-168.49479,-292.71547z"/>
+                <path fill="var(--cont_color_light)" id="hex5" d="M791.62372,179.13389l-169.19136,293.11723l338.31407,-0.18808z"/>
+            </svg>
+        `;
+
+        // Set hexagon colors
+        let side_counter = 0
+        for (let key in QUESTIONS_DATA) {
+            if (side_counter < 6) { // Hexagon has 6 sides
+                document.getElementById("hex"+side_counter).style.fill = "var(--"+QUESTIONS_DATA[key]+"_color)"
+            }
+            side_counter++;
         }
-        side_counter++;
     }
 
     // Get header cookies
@@ -207,7 +223,7 @@ function getHospitalData() {
                     </label>
                 </td>
                 <td class="hospital_radio_desc_cont">
-                    <p><span class="hospital_radio_desc_color">></span> <span id="${key}_description">${QUESTIONS[key][DATA[key]]}</span></p>
+                    <p id="${key}_description">${QUESTIONS[key][DATA[key]]}</p>
                 </td>
             </tr>
         `;
@@ -391,7 +407,9 @@ function update_table_and_header(table_headers=[],filtered_data=[]) {
             }
         }
 
-        row.innerHTML += `<td class="picture_column"> <img onclick="upload_pic(event, '`+person["first_name"]+` `+person["last_name"]+`')" src="/img/picture_icon.svg" height="35" alt="Last opp bilde"> </td>`; 
+        if (SHOW_PICTURE_ICON) {
+            row.innerHTML += `<td class="picture_column"> <img onclick="upload_pic(event, '`+person["first_name"]+` `+person["last_name"]+`')" src="/img/picture_icon.svg" height="35" alt="Last opp bilde"> </td>`; 
+        }
         
         if (person.comment != "") {
             row.innerHTML += `<td class="comment_column"> <img onclick="show_comment(event, '`+person.comment.replaceAll("'", "&#39;").replace(/[\u00A0-\u9999<>\u005C\u0026\u0022\u0027]/g, i => '&#' + i.charCodeAt(0) + ';')+`')" src="/img/comment_filled_icon.svg" height="35" alt="Se kommentar"> </td>`; 
